e46b4c40bbf0cc91f11ea06bcd0dd0fa52c729ea
umarsahab <121605324+umarsahab@users.noreply.github.com>
2023-03-04 19:55:09 +0500

    practise

---
+++ app.js
@@ -0,0 +1,105 @@
+
+
+
+// var cities = ['karachi' , 'lahore' , 'multan', 'islamabad']
+
+// // for(var i=0; i <= cities.length; i++){
+// //     console.log(cities[i])
+
+// // }
+
+// for(var i=cities.length - 1; i > -1; i--){
+//     console.log(cities[i])
+
+// }
+
+// for(var i=100; i > -1; i--){
+//     console.log(i)
+
+// }
+
+// // for(var i=1; i <= 10; i++){
+// //     console.log('2 *' + i + '=' + i*2)
+
+// // }
+
+
+// var tableNumber = +prompt('inter table number')
+// for(var i=1; i <= 10; i++){
+//     console.log(tableNumber +'*' + i + '=' + i*2)
+
+// }
+
+// var word = prompt("Enter your word");
+// var para = word.split("").reverse().join("");
+// // console.log(para)
+
+// if(word == para){
+//     document.write("your word is paradrome")
+// }
+// else{
+//     document.write("your word is not paradrome")
+// }
+
+
+
+// var userName = prompt('Inter your username')
+
+// for(var i=0; i < userName.length; i++ ){
+//     console.log(userName[i])
+
+//     if(userName[i] == ' '){
+//         alert('Plz do not inter space in username')
+//     }else{
+//         console.log('username is Valid')
+//         break;
+        
+//     }
+// }
+
+
+
+// var userName = prompt('Inter Your username')
+// var firstLetters = userName[0]
+// console.log(userName[0])
+
+// for (var i=0; i < userName.length; i++){
+   
+//   if(userName[i] == ' '){
+    
+//     console.log(userName[i + 1])
+//     firstLetters = firstLetters +' '+ userName[i + 1]
+    
+  
+// }
+// }
+// console.log(firstLetters)
+
+
+// var restuarents = {
+//     name: 'Zamzama'
+    
+// }
+// var numbers1 = [1,2,3,4,5,7,8,9,10]
+
+// numbers1
+// for(var i =0; i <= numbers1.length - 1; i++){
+   
+//     var currentNumber = numbers1[i]
+//     var nextNumber = numbers1[i + 1]
+//  console.log(currentNumber , nextNumber)
+
+//  if(currentNumber + 1 !== nextNumber){
+//     console.log('missing' , currentNumber + 1)
+//  }
+// }
+
+// var numbers = [1,2,3,20,8,15]
+
+// var largeNum = [0]
+
+// for(var i=1; i < numbers.length; i++){
+//    if(numbers[i] > largeNum)
+//    largeNum = numbers[i]
+// }
+// console.log(largeNum)
